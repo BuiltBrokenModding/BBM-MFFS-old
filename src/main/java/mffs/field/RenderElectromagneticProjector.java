f1b703d28c426a63f63703cdc73db58adc0d5dea
Francesco Macagno <fjmacagno1@yahoo.com>
2016-07-18 22:17:21 -0700

    Updated projector renderer

--- src/main/java/mffs/field/RenderElectromagneticProjector.java
+++ src/main/java/mffs/field/RenderElectromagneticProjector.java
@@ -1,110 +1,107 @@
-package mffs.field
+package mffs.field;
 
-import cpw.mods.fml.client.FMLClientHandler
-import cpw.mods.fml.relauncher.{Side, SideOnly}
-import mffs.render.FieldColor
-import mffs.{Reference, Settings}
-import net.minecraft.client.Minecraft
-import net.minecraft.client.renderer.{RenderHelper, Tessellator}
-import net.minecraft.util.ResourceLocation
-import net.minecraftforge.client.model.AdvancedModelLoader
-import org.lwjgl.opengl.GL11._
+import cpw.mods.fml.client.FMLClientHandler;
+import mffs.Reference;
+import mffs.Settings;
+import mffs.render.FieldColor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
 
-@SideOnly(Side.CLIENT)
-final object RenderElectromagneticProjector
-{
-  val textureOn = new ResourceLocation(Reference.domain, Reference.modelPath + "electromagneticProjector_on.png")
-  val textureOff = new ResourceLocation(Reference.domain, Reference.modelPath + "electromagneticProjector_off.png")
+final class RenderElectromagneticProjector {
+	private static ResourceLocation textureOn = new ResourceLocation(Reference.domain,
+			Reference.modelPath + "electromagneticProjector_on.png");
+	private static ResourceLocation textureOff = new ResourceLocation(Reference.domain,
+			Reference.modelPath + "electromagneticProjector_off.png");
 
-  val model = AdvancedModelLoader.loadModel(new ResourceLocation(Reference.domain, Reference.modelPath + "electromagneticProjector.tcn"))
+	private static IModelCustom model = AdvancedModelLoader
+			.loadModel(new ResourceLocation(Reference.domain, Reference.modelPath + "electromagneticProjector.tcn"));
 
-  def render(tileEntity: TileElectromagneticProjector, x: Double, y: Double, z: Double, frame: Float, isActive: Boolean, isItem: Boolean)
+	public void render(TileElectromagneticProjector tileEntity, Double x, Double y, Double z, Float frame, Boolean isActive, Boolean isItem)
   {
-    glPushMatrix
-    glTranslated(x + 0.5, y + 0.5, z + 0.5)
+    glPushMatrix();
+    glTranslated(x + 0.5, y + 0.5, z + 0.5);
 
     if (isActive)
-    {
-      FMLClientHandler.instance.getClient.renderEngine.bindTexture(textureOn)
-    }
+      FMLClientHandler.instance().getClient().renderEngine.bindTexture(textureOn);
     else
-    {
-      FMLClientHandler.instance.getClient.renderEngine.bindTexture(textureOff)
-    }
+      FMLClientHandler.instance().getClient().renderEngine.bindTexture(textureOff);
 
     if (!isItem)
     {
-      glRotatef(-90, 0, 1, 0)
-      RenderUtility.rotateBlockBasedOnDirection(tileEntity.getDirection)
+      glRotatef(-90, 0, 1, 0);
+      RenderUtility.rotateBlockBasedOnDirection(tileEntity.getDirection());
     }
 
-    model.renderAll
+    model.renderAll();
     //.render(tileEntity.animation, 0.0625F)
-    glPopMatrix
+    glPopMatrix();
 
-    if (tileEntity.getMode != null)
+    if (tileEntity.getMode() != null)
     {
-      val tessellator: Tessellator = Tessellator.instance
-      RenderHelper.disableStandardItemLighting
-      glPushMatrix
-      glTranslated(x + 0.5, y + 0.5, z + 0.5)
-      val xDifference: Double = Minecraft.getMinecraft.thePlayer.posX - (tileEntity.xCoord + 0.5)
-      val zDifference: Double = Minecraft.getMinecraft.thePlayer.posZ - (tileEntity.zCoord + 0.5)
-      val rotatation: Float = Math.toDegrees(Math.atan2(zDifference, xDifference)).asInstanceOf[Float]
-      glRotatef(-rotatation + 27, 0.0F, 1.0F, 0.0F)
-      glDisable(GL_TEXTURE_2D)
-      glShadeModel(GL_SMOOTH)
-      glEnable(GL_BLEND)
-      glBlendFunc(GL_SRC_ALPHA, GL_ONE)
-      glDisable(GL_ALPHA_TEST)
-      glEnable(GL_CULL_FACE)
-      glDepthMask(false)
-      glPushMatrix
-      tessellator.startDrawing(6)
-      val height: Float = 2
-      val width: Float = 2
-      tessellator.setColorRGBA(72, 198, 255, 255)
-      tessellator.addVertex(0.0D, 0.0D, 0.0D)
-      tessellator.setColorRGBA_I(0, 0)
-      tessellator.addVertex(-0.866D * width, height, -0.5F * width)
+      Tessellator tessellator = Tessellator.instance;
+      RenderHelper.disableStandardItemLighting();
+      glPushMatrix();
+      glTranslated(x + 0.5, y + 0.5, z + 0.5);
+      double xDifference = Minecraft.getMinecraft().thePlayer.posX - (tileEntity.xCoord + 0.5);
+      double zDifference = Minecraft.getMinecraft().thePlayer.posZ - (tileEntity.zCoord + 0.5);
+      float rotatation = (float) Math.toDegrees(Math.atan2(zDifference, xDifference));
+      glRotatef(-rotatation + 27, 0.0F, 1.0F, 0.0F);
+      glDisable(GL_TEXTURE_2D);
+      glShadeModel(GL_SMOOTH);
+      glEnable(GL_BLEND);
+      glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+      glDisable(GL_ALPHA_TEST);
+      glEnable(GL_CULL_FACE);
+      glDepthMask(false);
+      glPushMatrix();
+      tessellator.startDrawing(6);
+      float height = 2, width = 2;
+      tessellator.setColorRGBA(72, 198, 255, 255);
+      tessellator.addVertex(0.0D, 0.0D, 0.0D);
+      tessellator.setColorRGBA_I(0, 0);
+      tessellator.addVertex(-0.866D * width, height, -0.5F * width);
       tessellator.addVertex(0.866D * width, height, -0.5F * width)
       tessellator.addVertex(0.0D, height, 1.0F * width)
       tessellator.addVertex(-0.866D * width, height, -0.5F * width)
-      tessellator.draw
-      glPopMatrix
-      glDepthMask(true)
-      glDisable(GL_CULL_FACE)
-      glDisable(GL_BLEND)
-      glShadeModel(GL_FLAT)
-      glColor4f(1.0F, 1.0F, 1.0F, 1.0F)
-      glEnable(GL_TEXTURE_2D)
-      glEnable(GL_ALPHA_TEST)
-      RenderHelper.enableStandardItemLighting
-      glPopMatrix
+      tessellator.draw();
+      glPopMatrix();
+      glDepthMask(true);
+      glDisable(GL_CULL_FACE);
+      glDisable(GL_BLEND);
+      glShadeModel(GL_FLAT);
+      glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+      glEnable(GL_TEXTURE_2D);
+      glEnable(GL_ALPHA_TEST);
+      RenderHelper.enableStandardItemLighting();
+      glPopMatrix();
 
       /**
        * Render hologram
        */
       if (Settings.highGraphics)
       {
-        glPushMatrix
-        glTranslated(x + 0.5, y + 1.35, z + 0.5)
+        glPushMatrix();
+        glTranslated(x + 0.5, y + 1.35, z + 0.5);
 
-        val color = if (isActive) FieldColor.blue else FieldColor.red
-        FMLClientHandler.instance.getClient.renderEngine.bindTexture(Reference.hologramTexture)
+        FieldColor color = isActive ? FieldColor.blue : FieldColor.red;
+        FMLClientHandler.instance().getClient().renderEngine.bindTexture(Reference.hologramTexture);
 
-        RenderUtility.enableBlending
-        RenderUtility.disableLighting
-        glPushMatrix
-        glColor4d(color._1, color._2, color._3, Math.sin(tileEntity.getTicks.toDouble / 10) / 2 + 0.8)
-        glTranslatef(0, Math.sin(Math.toRadians(tileEntity.getTicks * 3)).toFloat / 7, 0)
-        glRotatef(tileEntity.getTicks * 4, 0, 1, 0)
-        glRotatef(36f + tileEntity.getTicks * 4, 0, 1, 1)
-        tileEntity.getMode.render(tileEntity, x, y, z, frame, tileEntity.getTicks)
-        glPopMatrix
-        RenderUtility.enableLighting
-        RenderUtility.disableBlending
-        glPopMatrix
+        RenderUtility.enableBlending();
+        RenderUtility.disableLighting();
+        glPushMatrix();
+        glColor4d(color._1, color._2, color._3, Math.sin(tileEntity.getTicks() / 10.0) / 2 + 0.8);
+        glTranslatef(0, Math.sin(Math.toRadians(tileEntity.getTicks() * 3)) / 7.0, 0);
+        glRotatef(tileEntity.getTicks() * 4, 0, 1, 0);
+        glRotatef(36f + tileEntity.getTicks() * 4, 0, 1, 1);
+        tileEntity.getMode().render(tileEntity, x, y, z, frame, tileEntity.getTicks());
+        glPopMatrix();
+        RenderUtility.enableLighting();
+        RenderUtility.disableBlending();
+        glPopMatrix();
       }
     }
 
--- src/main/java/mffs/render/FieldColor.java
+++ src/main/java/mffs/render/FieldColor.java
@@ -1,11 +1,14 @@
-package mffs.render
+package mffs.render;
 
-/**
- * @author Calclavia
- */
-object FieldColor
-{
-  val red = (1f, 0f, 0f)
-  val green = (0f, 1f, 0f)
-  val blue = (0.5f, 0.9f, 1f)
+public enum FieldColor {
+	RED(1f, 0f, 0f), GREEN(0f, 1f, 0f), BLUE(0.5f, 0.9f, 1f);
+
+	private float r, g, b;
+
+	private FieldColor(float r, float g, float b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+
 }
