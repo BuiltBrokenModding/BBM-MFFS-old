23564fe7230c7d907f6b85b31b4a9ada4d9f33c1
Robert Seifert <rseifert.phone@gmail.com>
2016-07-22 15:14:21 -0400

    Converted hologram effect classes

--- src/main/java/mffs/render/fx/FXHologram.java
+++ src/main/java/mffs/render/fx/FXHologram.java
@@ -1,79 +1,91 @@
-package mffs.render.fx
+package mffs.render.fx;
 
-import cpw.mods.fml.client.FMLClientHandler
-import cpw.mods.fml.relauncher.{Side, SideOnly}
-import mffs.Content
-import net.minecraft.client.particle.EntityFX
-import net.minecraft.client.renderer.{OpenGlHelper, RenderBlocks, Tessellator}
-import net.minecraft.world.World
-import org.lwjgl.opengl.GL11
+import com.builtbroken.mc.lib.render.RenderUtility;
+import com.builtbroken.mc.lib.transform.vector.Pos;
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import mffs.ModularForceFieldSystem;
+import net.minecraft.client.particle.EntityFX;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.world.World;
+import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
-public class FXHologram(par1World: World, position: Vector3, red: Float, green: Float, blue: Float, age: Int) extends FXMFFS(par1World, position.x, position.y, position.z)
+public class FXHologram extends FXMFFS
 {
-  private var targetPosition: Vector3 = null
+    private Pos targetPosition = null;
 
-  this.setRBGColorF(red, green, blue)
-  this.particleMaxAge = age
-  this.noClip = true
-
-  /**
-   * The target the hologram is going to translate to.
-   *
-   * @param targetPosition
-   * @return
-   */
-  def setTarget(targetPosition: Vector3): FXHologram =
-  {
-    this.targetPosition = targetPosition
-    this.motionX = (this.targetPosition.x - this.posX) / this.particleMaxAge
-    this.motionY = (this.targetPosition.y - this.posY) / this.particleMaxAge
-    this.motionZ = (this.targetPosition.z - this.posZ) / this.particleMaxAge
-    return this
-  }
-
-  override def onUpdate
-  {
-    super.onUpdate
-    this.prevPosX = this.posX
-    this.prevPosY = this.posY
-    this.prevPosZ = this.posZ
-
-    particleAge += 1
-
-    if (particleAge - 1 >= this.particleMaxAge)
+    public FXHologram(World world, Pos position, float red, float green, float blue, int age)
     {
-      this.setDead
-      return
+        super(world, position.x(), position.y(), position.z(), 0, 0, 0);
+        this.setRBGColorF(red, green, blue);
+        this.particleMaxAge = age;
+        this.noClip = true;
     }
-    if (this.targetPosition != null)
-    {
-      this.moveEntity(this.motionX, this.motionY, this.motionZ)
-    }
-  }
 
-  override def renderParticle(tessellator: Tessellator, f: Float, f1: Float, f2: Float, f3: Float, f4: Float, f5: Float)
-  {
-    tessellator.draw
-    GL11.glPushMatrix
-    val xx: Float = (this.prevPosX + (this.posX - this.prevPosX) * f - EntityFX.interpPosX).asInstanceOf[Float]
-    val yy: Float = (this.prevPosY + (this.posY - this.prevPosY) * f - EntityFX.interpPosY).asInstanceOf[Float]
-    val zz: Float = (this.prevPosZ + (this.posZ - this.prevPosZ) * f - EntityFX.interpPosZ).asInstanceOf[Float]
-    GL11.glTranslated(xx, yy, zz)
-    GL11.glScalef(1.01f, 1.01f, 1.01f)
-    var op: Float = 0.5f
-    if ((this.particleMaxAge - this.particleAge <= 4))
+
+    /**
+     * The target the hologram is going to translate to.
+     *
+     * @param targetPosition
+     * @return
+     */
+    public FXHologram setTarget(Pos targetPosition)
     {
-      op = 0.5f - (5 - (this.particleMaxAge - this.particleAge)) * 0.1F
+        this.targetPosition = targetPosition;
+        this.motionX = (this.targetPosition.x() - this.posX) / this.particleMaxAge;
+        this.motionY = (this.targetPosition.y() - this.posY) / this.particleMaxAge;
+        this.motionZ = (this.targetPosition.z() - this.posZ) / this.particleMaxAge;
+        return this;
     }
-    GL11.glColor4d(this.particleRed, this.particleGreen, this.particleBlue, op * 2)
-    OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F)
-    RenderUtility.enableBlending
-    RenderUtility.setTerrainTexture
-    RenderUtility.renderNormalBlockAsItem(Content.forceField, 0, new RenderBlocks)
-    RenderUtility.disableBlending
-    GL11.glPopMatrix
-    tessellator.startDrawingQuads
-    FMLClientHandler.instance.getClient.renderEngine.bindTexture(RenderUtility.PARTICLE_RESOURCE)
-  }
+
+    @Override
+    public void onUpdate()
+    {
+        super.onUpdate();
+        this.prevPosX = this.posX;
+        this.prevPosY = this.posY;
+        this.prevPosZ = this.posZ;
+
+        particleAge += 1;
+
+        if (particleAge - 1 >= this.particleMaxAge)
+        {
+            this.setDead();
+            return;
+        }
+        if (this.targetPosition != null)
+        {
+            this.moveEntity(this.motionX, this.motionY, this.motionZ);
+        }
+    }
+
+    @Override
+    public void renderParticle(Tessellator tessellator, float f, float f1, float f2, float f3, float f4, float f5)
+    {
+        tessellator.draw();
+        GL11.glPushMatrix();
+        float xx = (float) (this.prevPosX + (this.posX - this.prevPosX) * f - EntityFX.interpPosX);
+        float yy = (float) (this.prevPosY + (this.posY - this.prevPosY) * f - EntityFX.interpPosY);
+        float zz = (float) (this.prevPosZ + (this.posZ - this.prevPosZ) * f - EntityFX.interpPosZ);
+        GL11.glTranslated(xx, yy, zz);
+        GL11.glScalef(1.01f, 1.01f, 1.01f);
+        float op = 0.5f;
+        if ((this.particleMaxAge - this.particleAge <= 4))
+        {
+            op = 0.5f - (5 - (this.particleMaxAge - this.particleAge)) * 0.1F;
+        }
+        GL11.glColor4d(this.particleRed, this.particleGreen, this.particleBlue, op * 2);
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
+        RenderUtility.enableBlending();
+        RenderUtility.setTerrainTexture();
+        RenderUtility.renderNormalBlockAsItem(ModularForceFieldSystem.forceField, 0, new RenderBlocks());
+        RenderUtility.disableBlending();
+        GL11.glPopMatrix();
+        tessellator.startDrawingQuads();
+        FMLClientHandler.instance().getClient().renderEngine.bindTexture(RenderUtility.PARTICLE_RESOURCE);
+    }
 }
--- src/main/java/mffs/render/fx/FXHologramOrbit.java
+++ src/main/java/mffs/render/fx/FXHologramOrbit.java
@@ -1,59 +1,76 @@
-package mffs.render.fx
+package mffs.render.fx;
 
-import cpw.mods.fml.client.FMLClientHandler
-import cpw.mods.fml.relauncher.{Side, SideOnly}
-import mffs.Content
-import net.minecraft.client.particle.EntityFX
-import net.minecraft.client.renderer.{OpenGlHelper, RenderBlocks, Tessellator}
-import net.minecraft.world.World
-import org.lwjgl.opengl.GL11
+import com.builtbroken.mc.lib.render.RenderUtility;
+import com.builtbroken.mc.lib.transform.rotation.EulerAngle;
+import com.builtbroken.mc.lib.transform.vector.Pos;
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import mffs.ModularForceFieldSystem;
+import net.minecraft.client.particle.EntityFX;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.world.World;
+import org.lwjgl.opengl.GL11;
 
 /**
  * @author Calclavia
  */
 @SideOnly(Side.CLIENT)
-public class FXHologramOrbit(par1World: World, orbitPosition: Vector3, position: Vector3, red: Float, green: Float, blue: Float, age: Int, maxSpeed: Float) extends FXHologram(par1World, position, red, green, blue, age)
+public class FXHologramOrbit extends FXHologram
 {
-  private var rotation: Double = 0
+    private double rotation = 0;
+    private double maxSpeed = 0;
+    private Pos orbitPosition;
 
-  override def onUpdate
-  {
-    super.onUpdate
-    val xDifference: Double = this.posX - orbitPosition.x
-    val yDifference: Double = this.posY - orbitPosition.y
-    val zDifference: Double = this.posZ - orbitPosition.z
-    val speed: Double = this.maxSpeed * (this.particleAge.asInstanceOf[Float] / this.particleMaxAge.asInstanceOf[Float])
-    val originalPosition: Vector3 = new Vector3(this)
-    val relativePosition: Vector3 = originalPosition.clone.subtract(this.orbitPosition)
-    relativePosition.transform(new EulerAngle(speed, 0, 0))
-    val newPosition: Vector3 = this.orbitPosition.clone.add(relativePosition)
-    this.rotation += speed
-    this.moveEntity(newPosition.x - originalPosition.x, newPosition.y - originalPosition.y, newPosition.z - originalPosition.z)
-  }
-
-  override def renderParticle(tessellator: Tessellator, f: Float, f1: Float, f2: Float, f3: Float, f4: Float, f5: Float)
-  {
-    tessellator.draw
-    GL11.glPushMatrix
-    val xx: Float = (this.prevPosX + (this.posX - this.prevPosX) * f - EntityFX.interpPosX).asInstanceOf[Float]
-    val yy: Float = (this.prevPosY + (this.posY - this.prevPosY) * f - EntityFX.interpPosY).asInstanceOf[Float]
-    val zz: Float = (this.prevPosZ + (this.posZ - this.prevPosZ) * f - EntityFX.interpPosZ).asInstanceOf[Float]
-    GL11.glTranslated(xx, yy, zz)
-    GL11.glScalef(1.01f, 1.01f, 1.01f)
-    GL11.glRotated(-this.rotation, 0, 1, 0)
-    var op: Float = 0.5f
-    if ((this.particleMaxAge - this.particleAge <= 4))
+    public FXHologramOrbit(World world, Pos orbitPosition, Pos position, float red, float green, float blue, int age, float maxSpeed)
     {
-      op = 0.5f - (5 - (this.particleMaxAge - this.particleAge)) * 0.1F
+        super(world, position, red, green, blue, age);
+        this.orbitPosition = orbitPosition;
+        this.maxSpeed = maxSpeed;
     }
-    GL11.glColor4d(this.particleRed, this.particleGreen, this.particleBlue, op * 2)
-    OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F)
-    RenderUtility.enableBlending
-    RenderUtility.setTerrainTexture
-    RenderUtility.renderNormalBlockAsItem(Content.forceField, 0, new RenderBlocks)
-    RenderUtility.disableBlending
-    GL11.glPopMatrix
-    tessellator.startDrawingQuads
-    FMLClientHandler.instance.getClient.renderEngine.bindTexture(RenderUtility.PARTICLE_RESOURCE)
-  }
+
+    @Override
+    public void onUpdate()
+    {
+        super.onUpdate();
+        double xDifference = this.posX - orbitPosition.x();
+        double yDifference = this.posY - orbitPosition.y();
+        double zDifference = this.posZ - orbitPosition.z();
+        double speed = this.maxSpeed * ((float) this.particleAge / (float) this.particleMaxAge);
+        Pos originalPosition = new Pos(this);
+        Pos relativePosition = originalPosition.clone().subtract(this.orbitPosition);
+        relativePosition.transform(new EulerAngle(speed, 0, 0));
+        Pos newPosition = this.orbitPosition.clone().add(relativePosition);
+        this.rotation += speed;
+        this.moveEntity(newPosition.x() - originalPosition.x(), newPosition.y() - originalPosition.y(), newPosition.z() - originalPosition.z());
+    }
+
+    @Override
+    public void renderParticle(Tessellator tessellator, float f, float f1, float f2, float f3, float f4, float f5)
+    {
+        tessellator.draw();
+        GL11.glPushMatrix();
+        float xx = (float) (this.prevPosX + (this.posX - this.prevPosX) * f - EntityFX.interpPosX);
+        float yy = (float) (this.prevPosY + (this.posY - this.prevPosY) * f - EntityFX.interpPosY);
+        float zz = (float) (this.prevPosZ + (this.posZ - this.prevPosZ) * f - EntityFX.interpPosZ);
+        GL11.glTranslated(xx, yy, zz);
+        GL11.glScalef(1.01f, 1.01f, 1.01f);
+        GL11.glRotated(-this.rotation, 0, 1, 0);
+        float op = 0.5f;
+        if ((this.particleMaxAge - this.particleAge <= 4))
+        {
+            op = 0.5f - (5 - (this.particleMaxAge - this.particleAge)) * 0.1F;
+        }
+        GL11.glColor4d(this.particleRed, this.particleGreen, this.particleBlue, op * 2);
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
+        RenderUtility.enableBlending();
+        RenderUtility.setTerrainTexture();
+        RenderUtility.renderNormalBlockAsItem(ModularForceFieldSystem.forceField, 0, new RenderBlocks());
+        RenderUtility.disableBlending();
+        GL11.glPopMatrix();
+        tessellator.startDrawingQuads();
+        FMLClientHandler.instance().getClient().renderEngine.bindTexture(RenderUtility.PARTICLE_RESOURCE);
+    }
 }
