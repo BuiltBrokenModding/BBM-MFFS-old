2eeb38523b95036f3272a2d4e9d1bed5e14e06d4
Robert Seifert <rseifert.phone@gmail.com>
2016-07-23 15:46:59 -0400

    Converted ItemModePyramid

--- src/main/java/mffs/field/mode/ItemModePyramid.java
+++ src/main/java/mffs/field/mode/ItemModePyramid.java
@@ -1,119 +1,146 @@
-package mffs.field.mode
+package mffs.field.mode;
 
-import java.util.{HashSet, Set}
+import com.builtbroken.mc.lib.transform.region.Cube;
+import com.builtbroken.mc.lib.transform.rotation.EulerAngle;
+import com.builtbroken.mc.lib.transform.vector.Pos;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import mffs.api.machine.IFieldMatrix;
+import mffs.api.machine.IProjector;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.tileentity.TileEntity;
+import org.lwjgl.opengl.GL11;
 
-import cpw.mods.fml.relauncher.{Side, SideOnly}
-import net.minecraft.client.renderer.Tessellator
-import net.minecraft.tileentity.TileEntity
-import org.lwjgl.opengl.GL11
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemModePyramid extends ItemMode
 {
-  private val step = 1
+    private int step = 1;
 
-  def getExteriorPoints(projector: IFieldMatrix): Set[Vector3] =
-  {
-    val fieldBlocks = new HashSet[Vector3]
-
-    val posScale = projector.getPositiveScale
-    val negScale = projector.getNegativeScale
-
-    var xSize = Math.max(posScale.x, Math.abs(negScale.x))
-    var zSize = Math.max(posScale.z, Math.abs(negScale.z))
-    val ySize = Math.max(posScale.y, Math.abs(negScale.y)).asInstanceOf[Int]
-
-    val initX = xSize.asInstanceOf[Int]
-    val initZ = xSize.asInstanceOf[Int]
-
-    val xDecr = xSize / (ySize * 2)
-    val zDecr = zSize / (ySize * 2)
-
-    //Create pyramid
-    for (y <- -ySize to ySize)
+    @Override
+    public List<Pos> getExteriorPoints(IFieldMatrix projector)
     {
-      for (x <- -initX to initX; z <- -initZ to initZ)
-      {
-        if (Math.abs(x) == Math.round(xSize) && Math.abs(z) <= Math.round(zSize))
-          fieldBlocks.add(new Vector3(x, y, z))
-        else if (Math.abs(z) == Math.round(zSize) && Math.abs(x) <= Math.round(xSize))
-          fieldBlocks.add(new Vector3(x, y, z))
-        else if (y == -ySize)
-          fieldBlocks.add(new Vector3(x, y, z))
-      }
+        List<Pos> fieldBlocks = new ArrayList();
 
-      xSize -= xDecr
-      zSize -= zDecr
+        Pos posScale = projector.getPositiveScale();
+        Pos negScale = projector.getNegativeScale();
+
+        int xSize = Math.max(posScale.xi(), Math.abs(negScale.xi()));
+        int zSize = Math.max(posScale.zi(), Math.abs(negScale.zi()));
+        int ySize = Math.max(posScale.yi(), Math.abs(negScale.yi()));
+
+        int initX = xSize;
+        int initZ = xSize;
+
+        int xDecr = xSize / (ySize * 2);
+        int zDecr = zSize / (ySize * 2);
+
+        //Create pyramid
+        for (int y = -ySize; y <= ySize; y++)
+        {
+            for (int x = -initX; x <= initX; x++)
+            {
+                for (int z = -initZ; z <= initZ; z++)
+                {
+                    if (Math.abs(x) == Math.round(xSize) && Math.abs(z) <= Math.round(zSize))
+                    {
+                        fieldBlocks.add(new Pos(x, y, z));
+                    }
+                    else if (Math.abs(z) == Math.round(zSize) && Math.abs(x) <= Math.round(xSize))
+                    {
+                        fieldBlocks.add(new Pos(x, y, z));
+                    }
+                    else if (y == -ySize)
+                    {
+                        fieldBlocks.add(new Pos(x, y, z));
+                    }
+                }
+            }
+
+            xSize -= xDecr;
+            zSize -= zDecr;
+        }
+
+        return fieldBlocks;
     }
 
-    return fieldBlocks
-  }
-
-  def getInteriorPoints(projector: IFieldMatrix): Set[Vector3] =
-  {
-    val fieldBlocks: Set[Vector3] = new HashSet[Vector3]
-    val posScale: Vector3 = projector.getPositiveScale
-    val negScale: Vector3 = projector.getNegativeScale
-    val xStretch: Int = posScale.xi + negScale.xi
-    val yStretch: Int = posScale.yi + negScale.yi
-    val zStretch: Int = posScale.zi + negScale.zi
-    val translation = new Vector3(0, -0.4, 0)
-
-    for (x <- -xStretch to xStretch by step; y <- 0 to yStretch by step; z <- -zStretch to zStretch by step)
+    @Override
+    public List<Pos> getInteriorPoints(IFieldMatrix projector)
     {
-      val position = new Vector3(x, y, z) + translation
+        List<Pos> fieldBlocks = new ArrayList();
+        Pos posScale = projector.getPositiveScale();
+        Pos negScale = projector.getNegativeScale();
+        int xStretch = posScale.xi() + negScale.xi();
+        int yStretch = posScale.yi() + negScale.yi();
+        int zStretch = posScale.zi() + negScale.zi();
+        Pos translation = new Pos(0, -0.4, 0);
 
-      if (isInField(projector, position + new Vector3(projector.asInstanceOf[TileEntity])))
-      {
-        fieldBlocks.add(position)
-      }
-    }
-    return fieldBlocks
-  }
+        for (int y = -yStretch; y <= yStretch; y++)
+        {
+            for (int x = -xStretch; x <= xStretch; x++)
+            {
+                for (int z = -zStretch; z <= zStretch; z++)
+                {
+                    Pos position = new Pos(x, y, z).add(translation);
 
-  override def isInField(projector: IFieldMatrix, position: Vector3): Boolean =
-  {
-    val posScale: Vector3 = projector.getPositiveScale.clone
-    val negScale: Vector3 = projector.getNegativeScale.clone
-    val xStretch: Int = posScale.xi + negScale.xi
-    val yStretch: Int = posScale.yi + negScale.yi
-    val zStretch: Int = posScale.zi + negScale.zi
-    val projectorPos: Vector3 = new Vector3(projector.asInstanceOf[TileEntity])
-    projectorPos.add(projector.getTranslation)
-    projectorPos.add(new Vector3(0, -negScale.yi + 1, 0))
-    val relativePosition: Vector3 = position.clone.subtract(projectorPos)
-    relativePosition.transform(new EulerAngle(-projector.getRotationYaw, -projector.getRotationPitch, 0))
-    val region: Cuboid = new Cuboid(-negScale, posScale)
+                    if (isInField(projector, position.add(new Pos((TileEntity) projector))))
+                    {
+                        fieldBlocks.add(position);
+                    }
+                }
 
-    if (region.intersects(relativePosition) && relativePosition.y > 0)
-    {
-      if ((1 - (Math.abs(relativePosition.x) / xStretch) - (Math.abs(relativePosition.z) / zStretch) > relativePosition.y / yStretch))
-      {
-        return true
-      }
+            }
+        }
+        return fieldBlocks;
     }
 
-    return false
-  }
+    @Override
+    public boolean isInField(IFieldMatrix projector, Pos position)
+    {
+        Pos posScale = projector.getPositiveScale().clone();
+        Pos negScale = projector.getNegativeScale().clone();
+        int xStretch = posScale.xi() + negScale.xi();
+        int yStretch = posScale.yi() + negScale.yi();
+        int zStretch = posScale.zi() + negScale.zi();
+        Pos projectorPos = new Pos((TileEntity) projector);
+        projectorPos.add(projector.getTranslation());
+        projectorPos.add(new Pos(0, -negScale.yi() + 1, 0));
+        Pos relativePosition = position.clone().subtract(projectorPos);
+        relativePosition.transform(new EulerAngle(-projector.getRotationYaw(), -projector.getRotationPitch(), 0));
+        Cube region = new Cube(negScale.multiply(-1), posScale);
 
-  @SideOnly(Side.CLIENT) override def render(projector: IProjector, x: Double, y: Double, z: Double, f: Float, ticks: Long)
-  {
-    val tessellator = Tessellator.instance
-    GL11.glPushMatrix
-    GL11.glRotatef(180, 0, 0, 1)
-    val height: Float = 0.5f
-    val width: Float = 0.3f
-    val uvMaxX: Int = 2
-    val uvMaxY: Int = 2
-    val translation: Vector3 = new Vector3(0, -0.4, 0)
-    tessellator.startDrawing(6)
-    tessellator.setColorRGBA(72, 198, 255, 255)
-    tessellator.addVertexWithUV(0 + translation.x, 0 + translation.y, 0 + translation.z, 0, 0)
-    tessellator.addVertexWithUV(-width + translation.x, height + translation.y, -width + translation.z, -uvMaxX, -uvMaxY)
-    tessellator.addVertexWithUV(-width + translation.x, height + translation.y, width + translation.z, -uvMaxX, uvMaxY)
-    tessellator.addVertexWithUV(width + translation.x, height + translation.y, width + translation.z, uvMaxX, uvMaxY)
-    tessellator.addVertexWithUV(width + translation.x, height + translation.y, -width + translation.z, uvMaxX, -uvMaxY)
-    tessellator.addVertexWithUV(-width + translation.x, height + translation.y, -width + translation.z, -uvMaxX, -uvMaxY)
-    tessellator.draw
-    GL11.glPopMatrix
-  }
+        if (region.intersects(relativePosition) && relativePosition.y() > 0)
+        {
+            if ((1 - (Math.abs(relativePosition.x()) / xStretch) - (Math.abs(relativePosition.z()) / zStretch) > relativePosition.y() / yStretch))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void render(IProjector projector, double x, double y, double z, float f, long ticks)
+    {
+        Tessellator tessellator = Tessellator.instance;
+        GL11.glPushMatrix();
+        GL11.glRotatef(180, 0, 0, 1);
+        float height = 0.5f;
+        float width = 0.3f;
+        int uvMaxX = 2;
+        int uvMaxY = 2;
+        Pos translation = new Pos(0, -0.4, 0);
+        tessellator.startDrawing(6);
+        tessellator.setColorRGBA(72, 198, 255, 255);
+        tessellator.addVertexWithUV(0 + translation.x(), 0 + translation.y(), 0 + translation.z(), 0, 0);
+        tessellator.addVertexWithUV(-width + translation.x(), height + translation.y(), -width + translation.z(), -uvMaxX, -uvMaxY);
+        tessellator.addVertexWithUV(-width + translation.x(), height + translation.y(), width + translation.z(), -uvMaxX, uvMaxY);
+        tessellator.addVertexWithUV(width + translation.x(), height + translation.y(), width + translation.z(), uvMaxX, uvMaxY);
+        tessellator.addVertexWithUV(width + translation.x(), height + translation.y(), -width + translation.z(), uvMaxX, -uvMaxY);
+        tessellator.addVertexWithUV(-width + translation.x(), height + translation.y(), -width + translation.z(), -uvMaxX, -uvMaxY);
+        tessellator.draw();
+        GL11.glPopMatrix();
+    }
 }
