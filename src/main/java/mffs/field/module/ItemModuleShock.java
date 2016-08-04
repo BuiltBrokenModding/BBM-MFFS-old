99f7043090ed25eda33f825cde3697677539ee16
Robert Seifert <rseifert.phone@gmail.com>
2016-08-04 06:48:49 -0400

    Converted ItemModuleShock

--- src/main/java/mffs/field/module/ItemModuleShock.java
+++ src/main/java/mffs/field/module/ItemModuleShock.java
@@ -1,32 +1,35 @@
-package mffs.field.module
+package mffs.field.module;
 
-import mffs.ModularForceFieldSystem
-import mffs.base.ItemModule
-import mffs.field.TileForceField
-import mffs.security.MFFSPermissions
-import net.minecraft.entity.Entity
-import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.item.ItemStack
-import net.minecraft.world.World
+import mffs.ModularForceFieldSystem;
+import mffs.base.ItemModule;
+import mffs.field.TileForceField;
+import mffs.security.MFFSPermissions;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 public class ItemModuleShock extends ItemModule
 {
-  override def onCollideWithForceField(world: World, x: Int, y: Int, z: Int, entity: Entity, moduleStack: ItemStack): Boolean =
-  {
-    if (entity.isInstanceOf[EntityPlayer])
+    @Override
+    public boolean onCollideWithForceField(World world, int x, int y, int z, Entity entity, ItemStack moduleStack)
     {
-      val entityPlayer = entity.asInstanceOf[EntityPlayer]
-      val tile = world.getTileEntity(x, y, z)
+        if (entity instanceof EntityPlayer)
+        {
+            EntityPlayer entityPlayer = (EntityPlayer) entity;
+            TileEntity tile = world.getTileEntity(x, y, z);
 
-      if (tile.isInstanceOf[TileForceField])
-      {
-        if (tile.asInstanceOf[TileForceField].getProjector.hasPermission(entityPlayer.getGameProfile, MFFSPermissions.forceFieldWarp))
-          return true
-      }
+            if (tile instanceof TileForceField)
+            {
+                if (((TileForceField) tile).getProjector().hasPermission(entityPlayer.getGameProfile(), MFFSPermissions.forceFieldWarp))
+                {
+                    return true;
+                }
+            }
 
-      entity.attackEntityFrom(ModularForceFieldSystem.damageFieldShock, moduleStack.stackSize)
+            entity.attackEntityFrom(ModularForceFieldSystem.damageFieldShock, moduleStack.stackSize);
+        }
+        return true;
     }
-
-    return true
-  }
 }
