94ec0eeb50d5bb8684b9764b8fc3a119de2bf203
Robert Seifert <rseifert.phone@gmail.com>
2016-08-04 08:42:06 -0400

    Converted ItemModuleSponge

--- src/main/java/mffs/field/module/ItemModuleSponge.java
+++ src/main/java/mffs/field/module/ItemModuleSponge.java
@@ -1,43 +1,52 @@
-package mffs.field.module
+package mffs.field.module;
 
-import java.util.Set
+import com.builtbroken.mc.lib.transform.vector.Pos;
+import mffs.api.machine.IProjector;
+import mffs.base.ItemModule;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import net.minecraftforge.fluids.BlockFluidBase;
 
-import mffs.base.ItemModule
-import net.minecraft.block.BlockLiquid
-import net.minecraft.init.Blocks
-import net.minecraft.item.ItemStack
-import net.minecraft.tileentity.TileEntity
-import net.minecraftforge.fluids.BlockFluidBase
+import java.util.List;
 
 public class ItemModuleSponge extends ItemModule
 {
-  setMaxStackSize(1)
-
-  override def onProject(projector: IProjector, fields: Set[Vector3]): Boolean =
-  {
-    if (projector.getTicks % 60 == 0)
+    public ItemModuleSponge()
     {
-      val world = projector.asInstanceOf[TileEntity].getWorldObj
-
-      if (!world.isRemote)
-      {
-        for (point <- projector.getInteriorPoints)
-        {
-          val block = point.getBlock(world)
-
-          if (block.isInstanceOf[BlockLiquid] || block.isInstanceOf[BlockFluidBase])
-          {
-            point.setBlock(world, Blocks.air)
-          }
-        }
-      }
+        setMaxStackSize(1);
     }
 
-    return super.onProject(projector, fields)
-  }
+    @Override
+    public boolean onProject(IProjector projector, List<Pos> fields)
+    {
+        if (projector.getTicks() % 60 == 0)
+        {
+            World world = ((TileEntity) projector).getWorldObj();
 
-  override def requireTicks(moduleStack: ItemStack): Boolean =
-  {
-    return true
-  }
+            if (!world.isRemote)
+            {
+                for (Pos point : projector.getInteriorPoints())
+                {
+                    Block block = point.getBlock(world);
+
+                    if (block instanceof BlockLiquid || block instanceof BlockFluidBase)
+                    {
+                        point.setBlock(world, Blocks.air);
+                    }
+                }
+            }
+        }
+
+        return super.onProject(projector, fields);
+    }
+
+    @Override
+    public boolean requireTicks(ItemStack moduleStack)
+    {
+        return true;
+    }
 }
